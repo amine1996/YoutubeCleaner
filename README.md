754767eab44bf871f7b322cc4ea9701baa678f30
ISMAIL AMine <amine_ismail@hotmail.fr>
2017-09-17 19:52:18 +0200

    Changed event handling, added paper progress bar finish function wrapping and some youtubers

--- README.md
+++ README.md
@@ -2,4 +2,10 @@
 
 Simple greasemonkey script that removes video thumbnails from unwanted authors.
 
-Partially working at the moment : Bug when click on the youtube logo to go back (Not handling well the frame loading).
+Partially working at the moment : Can be improved to handle events more properly
+
+Two actions possible against garbage videos :
+DELETE or REPLACE (need to be changed manually in the script)
+
+Delete will simply remove the concerned elements.
+Replace will replace its name by Garbage and thumbnail by a picture of some garbage.
--- YoutubeCleaner.js
+++ YoutubeCleaner.js
@@ -13,12 +13,14 @@ var Actions = {
   DELETE: 1
 }
 
-var ACTION = Actions.DELETE;
+var ACTION = Actions.REPLACE;
 var GARBAGE_TITLE = 'Garbage';
 var GARBAGE_REAL_NAME = 'Garbage';
 var GARBAGE_THUMBNAIL = 'http://discardstudies.files.wordpress.com/2011/09/garbage_strike_queen_sherbourne_014.jpg';
+var GARBAGE_AUTHORS_SET;
+var WHILE_LOADING_GARBAGE_TIMER = 500;
 var GARBAGE_AUTHORS = [
-  GARBAGE_REAL_NAME, //Needed to refresh bugged elements
+  GARBAGE_REAL_NAME,
   'SQUEEZIE',
   'KENTIN',
   'NORMAN FAIT DES VIDÉOS',
@@ -38,17 +40,35 @@ var GARBAGE_AUTHORS = [
   'CYRIL /SUPERKONAR',
   'IbraPlus',
   'Doozy',
-  "Jojo Bernard"
+  'Jojo Bernard',
+  'IbraTV',
+  'McFly & Carlito',
+  'PUN',
+  'Néo The One',
+  'Panormal',
+  'Xenoss',
+  'Le Bled\'Art',
+  'Wankil Studio',
+  'LaSalle',
+  'Lama Faché',
+  'YoMax',
+  'PsYkO17',
+  'NORMAN chaîne secondaire',
+  'FlorianOnAir',
+  'Tibo InShape',
+  'Monsieur Faucon',
+  'CoD Forlan',
+  'Doozy',
+  'NICO'
 ];
 
 /*
  * Get all element with tags corresponding to a video thumbnail
  */
-function getVideoElements()
-{
+function getVideoElements() {
   //Normal videos
   var normalVideoElements = Array.prototype.slice.call(document.querySelectorAll('ytd-grid-video-renderer'));
-  
+
   //Recommandations
   var compactVideoElements = Array.prototype.slice.call(document.querySelectorAll('ytd-compact-video-renderer'));
   var videoElements = {
@@ -56,19 +76,18 @@ function getVideoElements()
     'compact': compactVideoElements
   };
   return videoElements;
-} 
+}
 
 /*
  * Create an array of video objects contains elements for the video author, video title and video image
  */
-function getVideoObjects(videoElements)
-{
+function getVideoObjects(videoElements) {
   var videoObjects = []
   videoElements['normal'].forEach(function (videoElem) {
     var videoElement = videoElem;
     var videoAuthor = videoElem.querySelector('[id=byline-container]');
-    var videoTitle = videoElem.getElementsByTagName('h3') [0];
-    var videoImage = videoElem.getElementsByTagName('img') [0];
+    var videoTitle = videoElem.getElementsByTagName('h3')[0];
+    var videoImage = videoElem.getElementsByTagName('img')[0];
     videoObjects.push({
       'element': videoElement,
       'author': videoAuthor,
@@ -80,7 +99,7 @@ function getVideoObjects(videoElements)
     var videoElement = videoElem;
     var videoAuthor = videoElem.querySelector('[id=byline]');
     var videoTitle = videoElem.querySelector('[id=video-title]');
-    var videoImage = videoElem.getElementsByTagName('img') [0];
+    var videoImage = videoElem.getElementsByTagName('img')[0];
     videoObjects.push({
       'element': videoElement,
       'author': videoAuthor,
@@ -88,32 +107,28 @@ function getVideoObjects(videoElements)
       'image': videoImage
     })
   });
-  
-  console.log(videoObjects);
+
   return videoObjects;
-} 
+}
 
 /*
  * For each video object that is garbage (existing in global_garbage array), change title, image and author or delete
  * caller : Event or function firing the function
  */
-function setGarbage(caller)
-{
-  console.log('Caller : '+caller+', Setting garbage...');
+function setGarbage(caller) {
+  console.log('Caller : ' + caller + ', Setting garbage...');
   var videoElements = getVideoElements();
   var videoObjects = getVideoObjects(videoElements);
   var garbageVideos = videoObjects.filter(function (garbageVideo) {
-    return GARBAGE_AUTHORS.indexOf(garbageVideo['author'].textContent.trim().toLowerCase()) !== - 1;
+    return GARBAGE_AUTHORS_SET.has(garbageVideo['author'].textContent.trim().toLowerCase());
   });
   garbageVideos.forEach(function (garbageVideo) {
-    if (ACTION == Actions.REPLACE)
-    {
+    if (ACTION == Actions.REPLACE) {
       garbageVideo['author'].textContent = GARBAGE_REAL_NAME;
       garbageVideo['image'].setAttribute('src', GARBAGE_THUMBNAIL);
       garbageVideo['title'].textContent = GARBAGE_TITLE;
-    } 
-    else if (ACTION == Actions.DELETE)
-    {
+    }
+    else if (ACTION == Actions.DELETE) {
       garbageVideo['element'].remove();
     }
   });
@@ -122,48 +137,37 @@ function setGarbage(caller)
 /*
  * Main function
  */
-function main()
-{
+function main() {
   //Set author list to lower case
   GARBAGE_AUTHORS = GARBAGE_AUTHORS.map(function (author) {
     return author.toLowerCase();
   });
-  
+
+  GARBAGE_AUTHORS_SET = new Set(GARBAGE_AUTHORS);
+
   //While loading change to garbage every 100ms
   var whileLoading = setInterval(function () {
     setGarbage("whileLoading");
-  }, 100);
-  
+  }, WHILE_LOADING_GARBAGE_TIMER);
+
   //After loading is finished
   window.addEventListener('load', function () {
-    //Stop setting garbage every 100ms
+    //Stop setting garbage every WHILE_LOADING_GARBAGE_TIMER
+    setGarbage("loading event");
     clearInterval(whileLoading);
-    
+
     //Add animation end event handling
     document.getElementsByTagName('ytd-app')[0].addEventListener('animationend', function () {
       setGarbage("animationEnd event");
     });
-   
-    //Wrapping afterNextRender function to set garbage
-    var nativeAfterNextRender = Polymer.RenderStatus.afterNextRender;
-    Polymer.RenderStatus.afterNextRender = function(){
-      nativeAfterNextRender.apply(this,arguments);
-      setGarbage("afterNextRender");
+
+    var paperProgressBar = Polymer.telemetry.registrations.filter(x => x.is == "yt-page-navigation-progress")[0];
+    var paperProgressBarFinishFn = paperProgressBar.finish;
+    paperProgressBar.finish = function () {
+      paperProgressBarFinishFn.apply(this, arguments);
+      setGarbage("paperProgressBarFinish");
     }
 
-    //Wrapping _atEndOfMicrotask function to set garbage with a 500ms timer to avoid spam
-    var lastSetGarbage = new Date().getTime();
-    var nativeAtEndOfMicrotask = Polymer.Async._atEndOfMicrotask;
-    Polymer.Async._atEndOfMicrotask = function(){
-      nativeAtEndOfMicrotask.apply(this,arguments);
-      
-      if(new Date().getTime() - lastSetGarbage > 500)
-      {
-        lastSetGarbage = new Date().getTime();
-        setGarbage("_atEndOfMicrotask");
-      }
-    }
-      
   });
 }
 
